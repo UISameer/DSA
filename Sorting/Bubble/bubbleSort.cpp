3e935f7f4e630cda8c003c19bddac58f18bcf709
Sameer Jagtap <jagtap.sameer00@gmail.com>
2021-03-14 20:57:10 +0530

    New algo's added.

--- Sorting/Bubble/bubbleSort.cpp
+++ Sorting/Bubble/bubbleSort.cpp
@@ -22,6 +22,30 @@ void bubbleSort(int arr[], int n) {
 	}
 }
 
+// An optimized version of Bubble Sort
+// optimized by stopping the algorithm if inner loop didn’t cause any swap.
+void bubbleSortOptimized(int arr[], int n)
+{
+	int i, j;
+	bool swapped;
+	for (i = 0; i < n - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(&arr[j], &arr[j + 1]);
+				swapped = true;
+			}
+		}
+
+		// IF no two elements were swapped by inner loop, then break
+		if (swapped == false)
+			break;
+	}
+}
+
 
 void printArray(int arr[], int size)
 {
